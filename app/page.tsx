d581386c6698955c0fc8b1de6e9c9c3ab270c8a9
VictoriaSto <v.stoichuk@gmail.com>
2023-10-31 21:29:38 -0400

    add height to global

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-3xl font-medium text-sky-800'>Test</p>
+    <>
+      <p className='text-3xl font-medium text-sky-800'>Test</p>
+      <Button variant={'destructive'}>
+        Try me
+      </Button>
+    </>
   )
 }
