843527ed9f279f545eb92fb001c866b2f80a5b63
VictoriaSto <v.stoichuk@gmail.com>
2023-11-01 13:49:01 -0400

    add logo component

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Logo from './logo'
 
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div className='h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm'>
+      <div className='p-6'>
+        <Logo />
+      </div>
     </div>
   )
 }
