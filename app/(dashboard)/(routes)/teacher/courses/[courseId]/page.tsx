72a7a3ca21eacebb54f03a27fd35f8f2b39d225b
VictoriaSto <v.stoichuk@gmail.com>
2023-11-03 18:45:40 -0400

    add fields to course page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,11 +1,52 @@
 import React from 'react'
+import { db } from "@/lib/db"
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-const CourseIdPage = ({ params } : {
+const CourseIdPage = async ({ params } : {
   params: { courseId: String }
 }) => {
+  const { userId } = auth()
+
+  if(!userId) {
+    return redirect("/")
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  })
+
+  if (!course) {
+    return redirect("/")
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+  ]
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`
+
   return (
-    <div>
-      Course ${params.courseId} Page
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            Course setup
+          </h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
