57327e30d9a538bc597ded0a58d0c997074c099b
VictoriaSto <v.stoichuk@gmail.com>
2023-11-03 19:52:46 -0400

    add iconBadge to customize option

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { db } from "@/lib/db"
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
+import { IconBadge } from '@/components/icon-badge'
+import { LayoutDashboard } from 'lucide-react'
 
 const CourseIdPage = async ({ params } : {
   params: { courseId: String }
@@ -50,6 +52,7 @@ const CourseIdPage = async ({ params } : {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div>
           <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard}/>
             <h2 className="text-xl">
               Customize your course
             </h2>
