4d0bc1f1f3433bc42fc7e17e67e86152a2a64bf6
VictoriaSto <v.stoichuk@gmail.com>
2023-11-04 18:41:55 -0400

    add title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,6 +1,27 @@
 "use client"
 import React from 'react'
 
+import * as z from "zod";
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required"
+  })
+})
+
 interface TitleFormProps {
   initialData: {
     title: string;
@@ -10,6 +31,17 @@ interface TitleFormProps {
 const TitleForm = ( {initialData, courseId } : 
   TitleFormProps
   ) => {
+    const from = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: initialData
+    });
+
+    const { isSubmitting, isValid } = from.formState;
+    
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+      console.log(values)
+    }
+
   return (
     <div>
       Title form
