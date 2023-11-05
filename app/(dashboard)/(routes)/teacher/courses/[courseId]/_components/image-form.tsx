fc471312dc0f1b71371a264faf59ad6709ec1166
VictoriaSto <v.stoichuk@gmail.com>
2023-11-05 13:27:53 -0500

    add style to upload

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -7,8 +7,10 @@ import * as z from "zod";
 import axios from 'axios';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { Pencil, PlusCircle } from 'lucide-react';
 import { Course } from '@prisma/client';
+import { Pencil, PlusCircle, ImageIcon } from 'lucide-react';
+import Image from "next/image";
+import { FileUpload } from "@/components/file-upload";
 
 import {
   Form,
@@ -83,45 +85,35 @@ const ImageForm = ( {initialData, courseId } :
         </Button>
       </div>
       {!isEditing && (
-        <p className={cn(
-          'text-sm mt-2',
-          !initialData.imageUrl && "text-slate-500 italic"
-        )}>
-          {initialData.imageUrl || "No image"}
-        </p>
+          !initialData.imageUrl ? (
+            <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+              <ImageIcon className="h-10 w-10 text-slate-500" />
+            </div>
+          ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+              src={initialData.imageUrl}
+            />
+          </div>
+          )
       )}
       {isEditing && (
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className='space-y-4 mt-4'
-          >
-            <FormField 
-              control={form.control}
-              name="image"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea 
-                      disabled={isSubmitting}
-                      placeholder="e.g. 'This course is about ...'"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className='flex items-center gap-x-2'>
-                <Button
-                  disabled={!isValid || isSubmitting}
-                  type="submit"
-                >
-                  Save
-                </Button>
-            </div>
-          </form>
-        </Form>
+        <div>
+          <FileUpload
+          endpoint="courseImage"
+          onChange={(url) => {
+            if (url) {
+              onSubmit({ imageUrl: url });
+            }
+          }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+          16:9 aspect ratio recommended
+          </div>
+        </div>
       )}
     </div>
   )
